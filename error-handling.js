3eb8ccab01873d866861c53d2243aac7ce2719a9
kevin ocansey <kevinocansey11@gmail.com>
2022-12-11 17:15:17 +0000

    Done with assignment

--- error-handling.js
+++ error-handling.js
@@ -1,5 +1,10 @@
 // 1.
 function sum(array) {
+  // create error within function
+  if(!(array instanceof Array)){
+    throw new Error('Type Error!. Input should be an array')
+  }
+
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
@@ -7,20 +12,47 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+// Try function and catch error
+try {
+  let res = sum(null);
+  console.log(res);
+} catch (error) {
+  console.error(error)
+}
+
 
 // 2.
-// tests
-sayName("Alex");
-sayName(1);
+
 // Your code here
 
-// 3.
+  function sayName (name){
+  if(typeof name !== "string"){
+    throw Error("Invalid name! Must be a string!")
+  }
+  console.log(`${name}`)
+}
+
+// tests
+  try {
+    sayName("Alex");
+    sayName(1);
+  } catch (error) {
+    console.log(error)
+  }
+
+
+3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
 
   console.log(greeting);
+}
+
+try {
+  greet()
+} catch (error) {
+  // console.log(error)
+  console.log('Hello World!')
 }
